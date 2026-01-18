d33d50ccc43da96d44b28bf3be6667328fa804f3
Mosap Abdel-Ghany <abdelghanymosap@gmail.com>
2026-01-18 21:11:16 +0200

    Create download_model.py

---
+++ download_model.py
@@ -0,0 +1,19 @@
+import gdown
+import zipfile
+import os
+
+FILE_ID = "1bOwBm-5MKnuvjNDdA83_EtFJUxzGnAPb"
+ZIP_NAME = "bart_mnli_model.zip"
+MODEL_DIR = "bart_mnli_model"
+
+if not os.path.exists(MODEL_DIR):
+    url = f"https://drive.google.com/uc?id={FILE_ID}"
+    gdown.download(url, ZIP_NAME, quiet=False)
+
+    with zipfile.ZipFile(ZIP_NAME, "r") as z:
+        z.extractall(".")
+
+    os.remove(ZIP_NAME)
+    print("model downloaded")
+else:
+    print("model already exists")
